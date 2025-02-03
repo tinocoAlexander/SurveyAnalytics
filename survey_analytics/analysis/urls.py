20705a94fff9bb7caa955d16cb922b3ddf9df6e6
tinocoAlexander <alexnuevo040@gmail.com>
2025-02-02 19:00:18 -0600

    Configuration: Routes

--- survey_analytics/analysis/urls.py
+++ survey_analytics/analysis/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'analysis'
+
 urlpatterns = [
+    path('analysisSurveys/', views.analysisSurveys, name='analysisSurveys'),
+    path('administrateSurveys/', views.administrateSurveys, name='administrateSurveys'),
 ]
--- survey_analytics/analysis/views.py
+++ survey_analytics/analysis/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def analysisSurveys(request):
+    title = "Analysis Surveys"
+    return render(request, 'analysis/analysisSurveys.html', {'title': title})
+
+def administrateSurveys(request):
+    title = "Administrate Surveys"
+    return render(request, 'analysis/administrateSurveys.html', {'title': title})

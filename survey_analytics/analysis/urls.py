05bb3fac7056fe4048f41069d2a4894fbb966e84
Alexander Tinoco Sanchez <alexnuevo040@gmail.com>
2025-02-08 00:08:24 -0600

    Routes protected

--- survey_analytics/analysis/urls.py
+++ survey_analytics/analysis/urls.py
@@ -7,4 +7,5 @@ app_name = 'analysis'
 urlpatterns = [
     path('analysisSurveys/', views.analysisSurveys, name='analysisSurveys'),
     path('lastSurveys/', views.lastSurveys, name='lastSurveys'),
+    path('resultSurveys/', views.resultSurveys, name='resultSurveys'),
 ]

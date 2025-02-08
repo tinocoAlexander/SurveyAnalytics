997974e0dc45ef04bbd14ceeaf9c25b87343612b
Alexander Tinoco Sanchez <alexnuevo040@gmail.com>
2025-02-08 01:05:36 -0600

    Login is functional

--- survey_analytics/users/forms.py
+++ survey_analytics/users/forms.py
@@ -1,7 +1,11 @@
 # users/forms.py
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.utils.text import capfirst
+from django.utils.translation import gettext_lazy as _
 
+# Formulario para el registro de usuarios
 class UserRegisterForm(forms.ModelForm):
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
@@ -38,3 +42,67 @@ class UserRegisterForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+
+
+from django import forms
+from django.contrib.auth import authenticate
+from django.utils.text import capfirst
+from django.utils.translation import gettext_lazy as _
+
+# Formulario para el inicio de sesión
+class AuthenticationForm(forms.Form):
+    username = forms.CharField(
+        max_length=254,
+        widget=forms.TextInput(attrs={
+            'autofocus': True,
+            'class': 'form-input',
+            'placeholder': 'Enter your username'
+        }),
+    )
+    password = forms.CharField(
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-input',
+            'placeholder': 'Enter your password',
+            'id': 'password',
+        }),
+    )
+
+    error_messages = {
+        'invalid_login': _(
+            "Please enter a correct %(username)s and password. Note that both fields may be case-sensitive."
+        ),
+        'inactive': _("This account is inactive."),
+    }
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user_cache = None
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = capfirst(self.fields['username'].label)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username is not None and password:
+            self.user_cache = authenticate(self.request, username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'username': self.fields['username'].label},
+                )
+            else:
+                self.confirm_login_allowed(self.user_cache)
+        return self.cleaned_data
+
+    def confirm_login_allowed(self, user):
+        if not user.is_active:
+            raise forms.ValidationError(
+                self.error_messages['inactive'],
+                code='inactive',
+            )
+
+    def get_user(self):
+        return self.user_cache
--- survey_analytics/users/views.py
+++ survey_analytics/users/views.py
@@ -1,14 +1,25 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
-from .forms import UserRegisterForm
+from django.contrib.auth import login as auth_login
+from .forms import UserRegisterForm, AuthenticationForm
 
 def index(request):
     title = "Homepage"
     return render(request, 'users/index.html', {'title': title})
 
+
 def login(request):
     title = "Login"
-    return render(request, 'users/login.html', {'title': title})
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('users:index')
+    else:
+        form = AuthenticationForm(request)
+
+    return render(request, 'users/login.html', {'title': title, 'form': form})
 
 def register(request):
     title = "Register"

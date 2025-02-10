e384ba6cb98f131702127b41a4d1d52662305f85
Alexander Tinoco Sanchez <alexnuevo040@gmail.com>
2025-02-10 00:19:56 -0600

    PDF download is functional

--- survey_analytics/analysis/urls.py
+++ survey_analytics/analysis/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('lastSurveys/', views.lastSurveys, name='lastSurveys'),
     path('resultSurveys/', views.resultSurveys, name='resultSurveys'),
     path('graphs/<int:survey_id>/', views.analysisGraphs, name='analysis_graphs'),
+    path('download_pdf/<int:survey_id>/', views.downloadPDF, name='download_pdf'),
 ]
